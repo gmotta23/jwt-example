26a5483e3bc6dea4cc783f18daee673ee8118ff4
Gustavo <gustavo.motta23@gmail.com>
2022-01-31 22:57:09 -0300

    TS habilitado

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+require('dotenv').config()
+import express, {Request, Response} from 'express'
+import morgan from 'morgan'
+
+const app = express()
+app.use(morgan('combined'))
+
+app.get('/test', (req, res: Response) => {
+  res.json('ok')
+})
+
+app.listen(process.env.SERVER_PORT, () => console.log('Listening on PORT ' + process.env.SERVER_PORT))
